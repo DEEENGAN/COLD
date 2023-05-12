5b4eea9126d634a24926b1496f8ce16b5fb6b128
Dee Engan <writing@deeengan.com>
2023-05-11 20:04:13 -0500

    ## main...origin/main D  configuration.nix M  flake.nix  M git-commit.dj A  time.nix

--- flake.nix
+++ flake.nix
@@ -5,14 +5,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   };
+
   outputs = { nixpkgs, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./configuration.nix
+        ./time.nix
 	    ./hardware-configuration.nix
 	    ./users.nix
         ./gnome.nix
+        #./gnome-theme.nix
         ./fonts.nix
         ./sound.nix
         ./service-timer-borg.nix
---
+++ time.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }: {
+
+    networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    networking.networkmanager.enable = true;
+
+    time.timeZone = "America/Chicago";
+
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "en_US.UTF-8";
+        LC_IDENTIFICATION = "en_US.UTF-8";
+        LC_MEASUREMENT = "en_US.UTF-8";
+        LC_MONETARY = "en_US.UTF-8";
+        LC_NAME = "en_US.UTF-8";
+        LC_NUMERIC = "en_US.UTF-8";
+        LC_PAPER = "en_US.UTF-8";
+        LC_TELEPHONE = "en_US.UTF-8";
+        LC_TIME = "en_US.UTF-8";
+};
+
+# Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+
+    system.stateVersion = "22.11";
+
+}
