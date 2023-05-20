cb9d13648a10c06ddcc20a0e3f6fc3ffdf8f4c08
Dee Engan <writing@deeengan.com>
2023-05-20 02:29:50 -0500

    M gnome.nix  M git-commit.dea ## main...origin/main

--- gnome.nix
+++ gnome.nix
@@ -43,10 +43,6 @@
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
     
-    # Chinese, needs /.config/ibus/rime with Cangjie6 https://github.com/rime-aca/rime-cangjie6
-    # i18n.inputMethod.enabled = "ibus";
-    # i18n.inputMethod.ibus.engines = with pkgs.ibus-engines; [ rime ];
-    
     # X11 windowing
     services.xserver.enable = true;
 
