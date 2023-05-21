df004780920790b47397d47e7c7919de1ba792d4
Dee Engan <writing@deeengan.com>
2023-05-20 22:27:11 -0500

    M nix-garbage-collect.nix  M gnome.nix  M git-commit.dea ## main...origin/main

--- nix-garbage-collect.nix
+++ nix-garbage-collect.nix
@@ -1,8 +1,11 @@
 { config, ... }: {
 
+# so, = "true"; and = true; are different -- the first throws up a "is not boolean" error, second does not
+#  A definition for option `nix.gc.persistent' is not of type `boolean'.
 
+nix.gc.automatic = true;
 nix.gc.randomizedDelaySec = "400";
-nix.gc.persistent = "true";
+nix.gc.persistent = true;
 nix.gc.options = "--delete-older-than 30d";
 nix.gc.dates = "Mon 04:30:00";
 
