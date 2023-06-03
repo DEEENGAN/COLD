b3a1916c4a7d3ab99bcf12c8ad52c5380696b205
Dee Engan <writing@deeengan.com>
2023-06-02 22:54:14 -0500

    refactor(fonts): correct CJK fonts

--- fonts.nix
+++ fonts.nix
@@ -1,13 +1,17 @@
 { config, pkgs, ... }: {
-  #fonts.fontconfig.enable = true;
+  fonts.fontconfig.enable = true;
+  fonts.fontDir.enable = true;
   #fonts.fontconfig.defaultFonts.emoji = [ "Twitter Color Emoji" ];
   #fonts.fontconfig.useEmbeddedBitmaps = true;
   fonts.fonts = with pkgs; [
-	  twitter-color-emoji
-	  noto-fonts-cjk-sans
-	  noto-fonts-cjk-serif
-    source-han-sans
-    source-han-serif
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      source-code-pro
+      source-han-mono
+      source-han-sans
+      source-han-serif
+      wqy_zenhei
     (nerdfonts.override { fonts = [ "Hack" "FantasqueSansMono" "Hasklig" ]; })
 	];
 
