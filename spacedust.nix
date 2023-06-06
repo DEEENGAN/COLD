56880197cc183ed996027e01674bb70bb633b778
Dee Engan <writing@deeengan.com>
2023-06-06 02:53:32 -0500

    feat(spacedust): new theme

---
+++ spacedust.nix
@@ -0,0 +1,24 @@
+{
+  normal.black = "#6E5346";
+  normal.red = "#E35B00";
+  normal.green = "#5CAB96";
+  normal.yellow = "#E3CD7B";
+  normal.blue = "#0F548B";
+  normal.magenta = "#E35B00";
+  normal.cyan = "#06AFC7";
+  normal.white = "#F0F1CE";
+
+  bright.black = "#684C31";
+  bright.red = "#FF8A3A";
+  bright.green = "#AECAB8";
+  bright.yellow = "#FFC878";
+  bright.blue = "#67A0CE";
+  bright.magenta = "#FF8A3A";
+  bright.cyan = "#83A7B4";
+  bright.white = "#FEFFF1";
+
+  primary.background = "#0A1E24";
+  primary.foreground = "#ECF0C1";
+
+  cursor.cursor = "#ECF0C1";
+}
